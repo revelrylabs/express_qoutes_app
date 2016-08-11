4ec2546fe0b04408641c466b6f8f7c127587c56e
Daniel Andrews <daniel@revelry.co>
2016-08-11 15:56:21 -0500

    add data

--- app.js
+++ app.js
@@ -5,16 +5,11 @@ var app = express()
 
 app.use(bodyParser.json())
 
-var quotes = [
-  { id: _.uniqueId(), author : 'Audrey Hepburn', text : "Nothing is impossible, the word itself says 'I'm possible'!"},
-  { id: _.uniqueId(), author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
-  { id: _.uniqueId(), author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
-  { id: _.uniqueId(), author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."}
-]
+var data = require('./data')
 
 // INDEX all quotes
-app.get('/quotes', (req, res) => {
-  res.json(quotes)
+app.get('/', (req, res) => {
+  res.json(data)
 })
 
 // SHOW a random quote
